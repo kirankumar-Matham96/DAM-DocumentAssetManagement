1921c152798787068d5f8ed2a041c340988e1c2b
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-11-08 19:36:58 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const { urlencoded } = require('express');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+	res.send('Welcome to DAM: Digital Asset Manager');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+	console.log(`Server is running at port number ${PORT}`);
+});
